4b273955bf245fee6fc19cdece7bc6fc5a959e1d
hangzhang <15038777234@163.com>
2015-06-25 12:02:54 +0800

    test

---
+++ ZHLib/ZHLibrary/ZHLibrary/ZHTest.h
@@ -0,0 +1,13 @@
+//
+//  ZHTest.h
+//  ZHLibrary
+//
+//  Created by hangzhang on 15/6/25.
+//  Copyright (c) 2015年 hangzhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZHTest : NSObject
+-(void)ZH_print;
+@end
---
+++ ZHLib/ZHLibrary/ZHLibrary/ZHTest.m
@@ -0,0 +1,16 @@
+//
+//  ZHTest.m
+//  ZHLibrary
+//
+//  Created by hangzhang on 15/6/25.
+//  Copyright (c) 2015年 hangzhang. All rights reserved.
+//
+
+#import "ZHTest.h"
+
+@implementation ZHTest
+-(void)ZH_print{
+
+    NSLog(@"name");
+}
+@end
